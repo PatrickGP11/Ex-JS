b0c34cf3475e0699497d90b460aaaea61658ea74
PatrickGP11 <patrickgonsalves06@gmail.com>
2025-04-01 19:27:06 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+const btn = document.getElementById("btn")
+
+function mudarConteudo() {
+    const titulo = document.querySelector(".titulo")
+
+    titulo.innerText = "Patrick"
+    titulo.classList.toggle("trocarFundo")
+}
+
+btn.addEventListener("click", mudarConteudo)
+
+/* const btn = document.getElementById("btn")
+
+nome = "Patrick"
+const cpf = "555.333.777-9"
+
+
+const teste = () => {
+    alert("teste")
+}
+
+function teste() {
+
+}
+
+
+btn.addEventListener("click", teste) */
+
+/* var btn = document.getElementById("btn") */
+
+/* console.log(btn) */
+
+
+/* btn.onclick = function() {
+    alert("Testando...")
+} */
+
+
+    /* function() */
+    /* function soma() */
+
+   /*  function soma() {
+        alert("Testando...")
+    }
+
+
+btn.addEventListener("click", ()=>{
+    alert("Testando...")
+    soma()
+}) */
